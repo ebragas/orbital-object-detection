782e4e83601940f0404c0e7d7ea47a8cf9a05cf2
Eric Bragas <ericbragas412@gmail.com>
2018-11-14 19:05:43 -0800

    Complete ML Engine job setup and training scripts

--- tensorflow-cnn/mnist_model/1_setup_mlengine.sh
+++ tensorflow-cnn/mnist_model/1_setup_mlengine.sh
@@ -1,1 +1,7 @@
-# gcloud ml-engine models create
+# Create model
+gcloud ml-engine models create mnist \
+    --regions=us-central1 \
+    --description='MNIST example model'
+
+# Delete model
+# gcloud ml-engine models delete mnist
--- tensorflow-cnn/mnist_model/3_train_mlengine.sh
+++ tensorflow-cnn/mnist_model/3_train_mlengine.sh
@@ -1,5 +1,21 @@
-gcloud ml-engine train \
+# Submission variables
+JOB_NAME=mnist_$(date +"%y%m%d_%H%M%S")
+OUTPUT_PATH=gs://reliable-realm-222318-mlengine/${JOB_NAME}
+
+# Submit job
+gcloud ml-engine jobs submit training $JOB_NAME \
+    --job-dir=$OUTPUT_PATH \
+    --runtime-version=1.8 \
     --module-name=trainer.task \
     --package-path=${PWD}/tensorflow-cnn/mnist_model/trainer \
+    --scale-tier=STANDARD_1 \
     -- \
-    --output_dir=output
+    --output_dir=output \
+    --train_steps=20000
+
+echo 'tensorboard --logdir' ${OUTPUT_PATH}
+
+
+# Cancel job
+# gcloud ml-engine jobs cancel $JOB_NAME
+# gcloud ml-engine jobs list
