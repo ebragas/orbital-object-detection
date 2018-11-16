e062b3482ee06ac8a9a6764fd5b395e6be3b2f26
Eric Bragas <ericbragas412@gmail.com>
2018-11-16 12:15:54 -0800

    Repurposed flowers model which I knew worked to satellite images. Saving this here for reference.

--- tensorflow-cnn/flowers_model/2_train_local.sh
+++ tensorflow-cnn/flowers_model/2_train_local.sh
@@ -4,11 +4,14 @@ gcloud ml-engine local train \
     --module-name=trainer.task \
     --package-path=${PWD}/tensorflow-cnn/${MODEL}/trainer \
     -- \
-    --output_dir=${PWD}/checkpoint \
+    --output_dir=${PWD}/tensorflow-cnn/${MODEL}/checkpoint \
     --train_steps=5 \
     --learning_rate=0.01 \
     --batch_size=2 \
     --model=cnn \
     --augment \
-    --train_data_path=gs://cloud-ml-data/img/flower_photos/train_set.csv \
-    --eval_data_path=gs://cloud-ml-data/img/flower_photos/eval_set.csv
+    --train_data_path=gs://reliable-realm-222318-vcm/satellite_imgs/csv/train_data.csv \
+    --eval_data_path=gs://reliable-realm-222318-vcm/satellite_imgs/csv/valid_data.csv
+
+    # --train_data_path=gs://cloud-ml-data/img/flower_photos/train_set.csv \
+    # --eval_data_path=gs://cloud-ml-data/img/flower_photos/eval_set.csv
--- tensorflow-cnn/flowers_model/3_train_mlengine.sh
+++ tensorflow-cnn/flowers_model/3_train_mlengine.sh
@@ -1,5 +1,5 @@
 # Parameters
-JOB_NAME=flowers_$(date +"%y%m%d_%H%M%S")
+JOB_NAME=flower_satellites_$(date +"%y%m%d_%H%M%S")
 MODEL_BUCKET=gs://reliable-realm-222318-mlengine
 DATA_BUCKET=gs://reliable-realm-222318-vcm
 OUTDIR=${MODEL_BUCKET}/${JOB_NAME}
@@ -18,10 +18,12 @@ gcloud ml-engine jobs submit training $JOB_NAME \
     --output_dir=$OUTDIR/output \
     --train_steps=1000 \
     --learning_rate=0.01 \
-    --batch_size=40 \
+    --batch_size=10 \
     --model=cnn \
-    --train_data_path=gs://cloud-ml-data/img/flower_photos/train_set.csv \
-    --eval_data_path=gs://cloud-ml-data/img/flower_photos/eval_set.csv
+    --train_data_path=gs://reliable-realm-222318-vcm/satellite_imgs/csv/train_data.csv \
+    --eval_data_path=gs://reliable-realm-222318-vcm/satellite_imgs/csv/valid_data.csv
+    # --train_data_path=gs://cloud-ml-data/img/flower_photos/train_set.csv \
+    # --eval_data_path=gs://cloud-ml-data/img/flower_photos/eval_set.csv
     # --augment \
     # --batch_norm \
 
