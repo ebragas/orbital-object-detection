8a574ea51ca3295aab04e7e753e74a5ef98b1be5
Eric Bragas <ericbragas412@gmail.com>
2018-11-20 14:53:42 -0800

    Transfer learning model deployment config changes and shell script for making predictions

--- tensorflow-cnn/satellite_transfer_model/3_deploy_model.sh
+++ tensorflow-cnn/satellite_transfer_model/3_deploy_model.sh
@@ -1,8 +1,8 @@
-MODEL_NAME=satellite
-MODEL_VERSION=v01
-MODEL_LOCATION=$(gsutil ls gs://reliable-realm-222318-mlengine/flower_satellites_181116_114324/output/export/exporter/ | tail -1)
+MODEL_NAME=satellite_transfer
+MODEL_VERSION=v02
+MODEL_LOCATION=$(gsutil ls gs://reliable-realm-222318-mlengine/satellite_transfer_181117_074615/output/export/exporter | tail -1)
 REGION=us-central1
-TFVERSION=1.8
+TFVERSION=1.10
 
 echo "Deleting and deploying $MODEL_NAME $MODEL_VERSION from $MODEL_LOCATION ... this will take a few minutes"
 
@@ -10,14 +10,15 @@ echo "Deleting and deploying $MODEL_NAME $MODEL_VERSION from $MODEL_LOCATION ...
 #gcloud ml-engine models delete ${MODEL_NAME}
 
 # create if doesn't exist
-gcloud ml-engine models create ${MODEL_NAME} \
-    --regions $REGION
-    --description 'Classify 80x80 images as "ship" or "no ship" from satellite imagery'
-    --enable-logging
+# gcloud ml-engine models create ${MODEL_NAME} \
+#     --regions $REGION \
+#     --description 'Image classification model using transfer learning' \
+#     --enable-logging
 
 # deploy SavedModel as version
 gcloud ml-engine versions create ${MODEL_VERSION} \
     --model ${MODEL_NAME} \
     --origin ${MODEL_LOCATION} \
-    --runtime-version=$TFVERSION
+    --runtime-version=$TFVERSION \
+    --python-version=3.5
 
--- tensorflow-cnn/satellite_transfer_model/4_predict.sh
+++ tensorflow-cnn/satellite_transfer_model/4_predict.sh
@@ -1,9 +1,10 @@
-MODEL_NAME=satellite
-MODEL_VERSION=v01
-REQUEST_PATH=${PWD}/tensorflow-cnn/satellite_model
+MODEL_NAME=satellite_transfer
+MODEL_VERSION=v02
+REQUEST_PATH=${PWD}/tensorflow-cnn/satellite_transfer_model
 
 # predict using local file
-IMG_PATH=/Users/ericbragas/galvanize/satellite-object-detection-case-study/data/imgs/no_ship/0__20150718_184300_090b__-122.40477488428849_37.8071040053892.png
+# IMG_PATH=/Users/ericbragas/galvanize/satellite-object-detection-case-study/data/imgs/no_ship/0__20150718_184300_090b__-122.40477488428849_37.8071040053892.png
+IMG_PATH=/Users/ericbragas/galvanize/satellite-object-detection-case-study/data/imgs/ship/1__20170522_180635_0f42__-122.33633731909474_37.75812175989443.png
 
 # base 64 encode and create request msg in json format
 python2 -c 'import base64, sys, json; img = base64.b64encode(open(sys.argv[1], "rb").read()); print json.dumps({"image_bytes": {"b64": img}})' $IMG_PATH &> ${REQUEST_PATH}/request.json
