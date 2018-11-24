d0b5e91b398cd5fc1f1aee3008639e0434fbe125
Eric Bragas <ericbragas412@gmail.com>
2018-11-24 09:20:07 -0800

    Rebuild weather example into gallery page.

--- webapp/main.py
+++ webapp/main.py
@@ -4,38 +4,27 @@ import logging
 
 from flask import Flask, flash, redirect, render_template, request, url_for
 from weather import query_api
+# from google.cloud import storage
 
+
+PROJECT = 'reliable-realm-222318'
+BUCKET = 'reliable-realm-222318-vcm'
 
 app = Flask(__name__)
 
 
-cities = [{'name': 'Toronto'}, {'name': 'Montreal'}, {'name': 'Calgary'},
-          {'name': 'Ottawa'}, {'name': 'Edmonton'}, {'name': 'Mississauga'},
-          {'name': 'Winnipeg'}, {'name': 'Vancouver'}, {'name': 'Brampton'},
-          {'name': 'Quebec'}, {'name': 'San Francisco'}]
+def get_storage_blobs(project, bucket_name, dir_prefix):
+    '''Return list of blob objects from the specified location
+    '''
+    client = storage.Client(project=project)
+    bucket = client.get_bucket(bucket_name=bucket_name)
+    blobs = list(bucket.list_blobs(prefix=dir_prefix))
+    return blobs
 
 @app.route('/')
 def index():
     """Return home page"""
-    return render_template('weather.html', data=cities)
-
-
-@app.route('/result', methods=['GET', 'POST'])
-def result():
-    data = []
-    error = None
-    select = request.form.get('comp_select') # NOTE: where does this come from?
-
-    response = query_api(select)
-    print(response)
-
-    if response:
-        data.append(response)
-    if len(data) != 2:
-        error = 'Bad response from weather API'
-    elif not response.get('weather', None):
-        error = 'Missing key {}'.format(response.keys())
-    return render_template('result.html', data=data, error=error)
+    return render_template('index.html')
 
 
 @app.errorhandler(500)
@@ -51,4 +40,3 @@ if __name__ == '__main__':
     # This is used when running locally. Gunicorn is used to run the
     # application on Google App Engine. See entrypoint in app.yaml.
     app.run(host='127.0.0.1', port=8080, debug=True)
-# [END gae_flex_quickstart]
